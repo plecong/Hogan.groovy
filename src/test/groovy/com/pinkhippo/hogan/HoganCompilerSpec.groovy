59d25b58c415f0b86a85e872e0dc5645970d339a
Phuong LeCong <phuong@lecong.org>
2012-04-16 00:13:33 -0700

    Fix syntax error in README Add license to source files.

--- src/test/groovy/com/pinkhippo/hogan/HoganCompilerSpec.groovy
+++ src/test/groovy/com/pinkhippo/hogan/HoganCompilerSpec.groovy
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2012 Phuong LeCong
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.pinkhippo.hogan
 
 import spock.lang.*
@@ -688,14 +703,14 @@ class HoganCompilerSpec extends Specification {
 			desc: 'Shootout Recurse compiled correctly',
 			text: '{{name}}{{#kids}}{{>recursion}}{{/kids}}',
 			data: [ name: '1',
-                kids: [
-                  [
-                    name: '1.1',
-                    kids: [
-                      [ name: '1.1.1', kids: [] ]
-                    ]
-                  ]
-            ]],
+				kids: [
+				  [
+					name: '1.1',
+					kids: [
+					  [ name: '1.1.1', kids: [] ]
+					]
+				  ]
+			]],
 			partials: [recursion: Hogan.compile("{{name}}{{#kids}}{{>recursion}}{{/kids}}")],
 			expected: '11.11.1.1'
 		],[
@@ -703,14 +718,14 @@ class HoganCompilerSpec extends Specification {
 			desc: 'Shootout Recurse string compiled correctly',
 			text: '{{name}}{{#kids}}{{>recursion}}{{/kids}}',
 			data: [ name: '1',
-                kids: [
-                  [
-                    name: '1.1',
-                    kids: [
-                      [ name: '1.1.1', kids: [] ]
-                    ]
-                  ]
-            ]],
+				kids: [
+				  [
+					name: '1.1',
+					kids: [
+					  [ name: '1.1.1', kids: [] ]
+					]
+				  ]
+			]],
 			partials: [recursion: "{{name}}{{#kids}}{{>recursion}}{{/kids}}"],
 			expected: '11.11.1.1'
 		],[
@@ -724,21 +739,21 @@ class HoganCompilerSpec extends Specification {
 			name: 'Shoot Out Complex',
 			desc: 'Shootout Complex compiled correctly',
 			text: "<h1>{{header}}</h1>" +
-			    "{{#hasItems}}" +
-			    "<ul>" +
-			      "{{#items}}" +
-			        "{{#current}}" +
-			          "<li><strong>{{name}}</strong></li>" +
-			        "{{/current}}" +
-			        "{{^current}}" +
-			          "<li><a href=\"{{url}}\">{{name}}</a></li>" +
-			        "{{/current}}" +
-			      "{{/items}}" +
-			    "</ul>" +
-			    "{{/hasItems}}" +
-			    "{{^hasItems}}" +
-			      "<p>The list is empty.</p>" +
-			    "{{/hasItems}}",
+				"{{#hasItems}}" +
+				"<ul>" +
+				  "{{#items}}" +
+					"{{#current}}" +
+					  "<li><strong>{{name}}</strong></li>" +
+					"{{/current}}" +
+					"{{^current}}" +
+					  "<li><a href=\"{{url}}\">{{name}}</a></li>" +
+					"{{/current}}" +
+				  "{{/items}}" +
+				"</ul>" +
+				"{{/hasItems}}" +
+				"{{^hasItems}}" +
+				  "<p>The list is empty.</p>" +
+				"{{/hasItems}}",
 			data: [
 				header: {
 					return 'Colors'
